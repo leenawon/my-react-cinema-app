305fc6ffc67b7b48e7e80f866912bb15fb2982e5
leenawon <nawon5154@gmail.com>
2021-11-20 18:44:41 +0900

    Loading state 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
       <h1>my react project</h1>
+      {loading ? <h3>Loading...</h3> : null}
     </div>
   );
 }
